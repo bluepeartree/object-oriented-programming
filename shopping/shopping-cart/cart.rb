2ba6138c7b9652d3005039709c68294cc4cd90f1
bluepeartree <sherrie.sebastian@gmail.com>
2017-11-08 17:23:27 -0500

    shopping cart in progress

--- shopping/shopping-cart/cart.rb
+++ shopping/shopping-cart/cart.rb
@@ -2,11 +2,89 @@ require './product.rb'
 
 class Cart
 
+@@Cart=[]
 
+def initialize
+  @total_before_tax = 0
+  @total_after_tax = 0
+end
 
+def total_before_tax
+  @total_before_tax
+end
 
+def total_after_tax
+  @total_after_tax
+end
 
+def total_before_tax=(total_before_tax)
+  @total_before_tax=total_before_tax
+end
 
+def total_after_tax=(total_after_tax)
+  @total_after_tax=total_after_tax
+end
+
+def self.all
+  @@Cart
+end
+
+def shopping_decision
+puts "Welcome to Lame Store!"
+puts "What would you like to do?"
+puts "add item / remove item / leave"
+response = gets.chomp
+  if response == "add"
+    add_to_cart
+  elsif response = "remove" && @@Cart.length < 1
+    puts "Your cart is already empty!"
+  elsif response = "remove" && @@Cart.length >= 1
+    remove_from_cart
+  else
+    puts "Thank you for your visit!"
+  end
+end
+
+def add_to_cart
+  puts "Our list of products is as follows:"
+  Product.listings
+  puts "What would you like to buy? "
+  response = gets.chomp
+  if Product.all_names.index(response)==nil
+    puts "Sorry, product not found."
+  else
+    Product.all.each do |product|
+      if product.name == response
+        @@Cart << product
+        puts @@Cart.inspect
+      end
+    end
+  end
+end
+
+def remove_from_cart
+  puts "What would you like to remove? "
+  response = gets.chomp
+  Cart.all.each do |product|
+      if product.name == response
+        @@Cart.delete(product)
+        puts @@Cart.inspect
+      end
+    end
+end
+
+def self.pre_tax_calc
+  Cart.all.each do |product|
+    @total_before_tax = product.base_price
+  end
+  return pre_tax_calc
+end
 
 
 end
+
+new_cart = Cart.new
+new_cart.shopping_decision
+new_cart.shopping_decision
+new_cart.shopping_decision
+Cartok.pre_tax_calc
--- shopping/shopping-cart/product.rb
+++ shopping/shopping-cart/product.rb
@@ -1,6 +1,7 @@
 class Product
 
-  
+@@Product_list =[]
+@@Product_list_names =[]
 
 def initialize (name, base_price)
   @name = name
@@ -17,6 +18,16 @@ def base_price
   @base_price
 end
 
+def tax_rate
+  @tax_rate
+end
+
+def total_price
+  @total_price
+end
+
+
+
 def name=(name)
   @name=name
 end
@@ -25,13 +36,41 @@ def base_price=(base_price)
   @base_price=base_price
 end
 
+def self.listings
+  @@Product_list.each do |item|
+    puts "Item name: #{item.name}"
+    puts "Price: $#{item.base_price}"
+  end
+end
 
-def price_calc
+
+def self.create(name, price)
+  new_product = Product.new(name, price)
+  @@Product_list << new_product
+  @@Product_list_names << new_product.name
+  return new_product
+end
+
+def self.all
+  @@Product_list
+end
+
+def self.all_names
+  @@Product_list_names
+end
+
+def total_price=(total_price)
   @total_price = @base_price + (@base_price*@tax_rate)
 end
 
 end
 
-product1 = Product.new("Book", 20)
-product2 = Product.new("Mop", 10)
-product3 = Product.new("Laptop", 1500)
+Product.create("Book", 20)
+Product.create("Mop", 10)
+Product.create("Laptop", 1500)
+#
+# puts Product.all.inspect
+# puts Product.all_names.inspect
+# Product.listings
+
+# puts Product.all[0].name
